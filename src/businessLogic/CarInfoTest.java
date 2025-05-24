ec2df08b8e0aab2e34fb658f124aa511f390cd2b
Notebook <Notebook@DESKTOP-VBH1A5U>
2025-05-24 14:39:17 -0500

    carInfoTest is the main class now

--- src/businessLogic/CarInfoTest.java
+++ src/businessLogic/CarInfoTest.java
@@ -6,7 +6,7 @@ public class CarInfoTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CarsReader cr = new CarsReader("Cars.csv");
+        CarsReader cr = new CarsReader("cars.csv");
         cr.readingData();
         cr.showCars();
     }
