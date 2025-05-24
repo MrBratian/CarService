c66b524ee714abaf9d6e11903549b8c40a9ecf39
Notebook <Notebook@DESKTOP-VBH1A5U>
2025-05-24 16:03:39 -0500

    minor changes

--- src/businessLogic/CarInfoTest.java
+++ src/businessLogic/CarInfoTest.java
@@ -1,14 +1,9 @@
 package businessLogic;
 
 public class CarInfoTest {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         CarsReader cr = new CarsReader("cars.csv");
         cr.readingData();
         cr.showCars();
     }
-    
 }
