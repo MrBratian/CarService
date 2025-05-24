0ede159b3283e457d50bdbf460894fd2bc0c2e37
Notebook <Notebook@DESKTOP-VBH1A5U>
2025-05-24 13:46:45 -0500

    cars.csv directory reader changed, added to same directory that the other classes

--- src/businessLogic/BusinessLogic.java
+++ src/businessLogic/BusinessLogic.java
@@ -6,7 +6,7 @@ public class BusinessLogic {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CarsReader cr = new CarsReader("C:\\Users\\danie\\Documents\\NetBeansProjects\\Cars.csv");
+        CarsReader cr = new CarsReader("Cars.csv");
         cr.readingData();
         cr.showCars();
     }
